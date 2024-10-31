ec02faea5490cbf4302ff1be723a54370060797c
Markok-code <gospodinkomaksim4@gmail.com>
2024-10-31 17:19:18 +0200

    Add files via upload

---
+++ 18_1.js
@@ -0,0 +1,15 @@
+const setTime = document.querySelector(".p")
+let time = parseInt(prompt())
+
+const timer = setInterval(() => {
+    let min = Math.trunc(time / 60) 
+    let sec =  time % 60
+    if (time >= 0) {
+        setTime.innerHTML = `${min}:${sec}`
+        time--
+    } 
+    else {
+        setTime.innerHTML = ""
+        clearInterval(timer)
+    }
+}, 100)
